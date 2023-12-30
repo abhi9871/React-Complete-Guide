e5ef4ff6bfa35c69bcb803fe8bf023bfcf69ece4
abhi9871 <at719273@gmail.com>
2023-12-30 21:05:15 +0530

    Task-2: Add css and js in ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,20 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return (
-        <div>
-        <h2>Expense Item!</h2>
-        <h3>Food Rs 10</h3>
-        <h3>Petrol Rs 100</h3>
-        <h3>Movies Rs 200</h3>
-        </div>
-    )
+  const expenseDate = new Date(2023, 11, 30);
+  const expenseTitle = "Car Insurance";
+  const expensePrice = 294.67;
+  const locationOfExpenditure = "Noida";
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h2>{locationOfExpenditure}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
