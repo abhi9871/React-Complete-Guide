437ebd34f064a3ee57eced17fb1077e679b7a806
abhi9871 <at719273@gmail.com>
2024-01-01 19:07:54 +0530

    Added click event listener for delete expense

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,12 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+
+  // Click handler function for delete
+  const deleteHandler = (e) => {
+    return e.target.parentElement.remove();
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -12,6 +18,7 @@ const ExpenseItem = (props) => {
         location={props.location}
         amount={props.amount}
       />
+      <button onClick={deleteHandler}>Delete Button</button>
     </Card>
   );
 }
